85f7974d17d3ab184940a137a053e955f9379d2d
commander-clickcluck <clickcluck321@gmail.com>
2025-05-12 13:45:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-AFFILIATE_TAG = os.getenv("AMAZON_ASSOCIATE_TAG")
-
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-@bot.command()
-async def affiliate(ctx, *, url: str):
-    if "amazon." not in url:
-        await ctx.send("Please provide a valid Amazon link.")
-        return
-
-    if "tag=" in url:
-        await ctx.send("Affiliate link already exists.")
-    else:
-        sep = "&" if "?" in url else "?"
-        affiliate_link = f"{url}{sep}tag={AFFILIATE_TAG}"
-        await ctx.send(f"Affiliate link: {affiliate_link}")
-
-bot.run(TOKEN)
+$ python main.py
+[2025-05-12 13:40:57] [WARNING ] discord.ext.commands.bot: Privileged message content intent is missing, commands may not work as expected.
+[2025-05-12 13:40:57] [INFO    ] discord.client: logging in using static token
+[2025-05-12 13:40:58] [INFO    ] discord.gateway: Shard ID None has connected to Gateway (Session ID: 206df3f266245354a4ade318168cce75).
+[2025-05-12 13:41:59] [INFO    ] discord.gateway: Shard ID None has successfully RESUMED session 206df3f266245354a4ade318168cce75.
